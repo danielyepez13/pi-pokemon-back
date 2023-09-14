7a6f1b408a3f8d47f0b2f357c83bae07a7b287ab
danielyepez13 <danielyepez62@gmail.com>
2023-09-14 09:43:03 -0400

    feat: start server

--- index.js
+++ index.js
@@ -17,12 +17,12 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js'); // Importamos configuracion del servidor
-const { conn } = require('./src/db.js'); // Importamos configuracion de la base de datos
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('%s listening at 3001');
   });
 });
